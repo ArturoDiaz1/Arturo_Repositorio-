0819001c9a190146ace889dac002abc703c25135
Arturo Pérez <wjoseperez65@gmail.com>
2020-09-16 01:31:52 -0400

    commit para responder el anterior

--- arturoPractica.c
+++ arturoPractica.c
@@ -4,5 +4,6 @@ int main()
 {
     printf("Este es el mejor código, ahora quiere ser el mejor en lo que hace nojoda");
     //Ahora Arturo1 y arturo2 prodrán trabajar mucho mejor juntos wey que bello
+    //Calro que si Amigo mio por cierto soy arturo2 jajajaja 
     return 0;
 }
