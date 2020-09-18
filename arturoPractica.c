5064b05afd5d47a2b6da8800c03a72ac952ff713
Arturo Pérez <wjoseperez65@gmail.com>
2020-09-18 16:19:16 -0400

    Master 1

--- arturoPractica.c
+++ arturoPractica.c
@@ -4,6 +4,7 @@ int main()
 {
     printf("Este es el mejor código, ahora quiere ser el mejor en lo que hace nojoda");
     //Ahora Arturo1 y arturo2 prodrán trabajar mucho mejor juntos wey que bello
-    //Calro que si Amigo mio por cierto soy arturo2 jajajaja 
+    //Calro que si Amigo mio por cierto soy arturo2 jajajaja
+    //master 1
     return 0;
 }
